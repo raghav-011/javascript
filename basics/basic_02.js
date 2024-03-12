b1316c0f7f2fc8bfc959a19ded088c4a1e213ae6
raghav-011 <115463546+raghav-011@users.noreply.github.com>
2024-03-12 15:16:45 +0000

    Datatypes and ECMA standard

---
+++ basics/basic_02.js
@@ -0,0 +1,26 @@
+console.log("this is the second tutorial of javascript")
+
+/* old js features are different from the latest or new js like in old js, classes, module were not exist
+and huge code written in old js, so the aim was that old js and new js will get same standard, so we use the following statement*/
+
+"use strict"; //now, complete code will treat as a new version of js
+
+// alert("hello") //is a built in function used to dispaly, a message in a dialog box commonly used for  playing information/ message to users
+
+//ECMA:- European Computer Manufracturer Associations is a standard organization based in Europe, that is responsible for developing standards
+
+//DataTypes:- are the type of the data used to assign the values, and give the information that what type of value, variable is storing
+
+let name = "raghav" //string datatype
+let age = 21 // numeric datatype
+let isLogged = false // bool datatype
+
+// undefined, null, object other types of it 
+
+console.log(typeof "raghu"); // typeof shows what type of the value
+console.log(typeof age); // also shows what type of value, variable stored
+
+console.log(typeof null); // object 
+console.log(typeof undefined); //undefined 
+
+// because undefined is a typs in its own while null is an object 
